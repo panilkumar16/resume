69940a73fd55a28297c89f5ad260dea8d46bc37e
Anil Kumar <panilkumar16@gmail.com>
2017-09-22 19:04:49 -0700

    Added all resume sections work, projects, biography, education to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,15 +2,124 @@
 Code to build the resume will go here.
  */
 
-var name = "Anilkumar P";
-var role = "Full Stack Software Engineer & Lead";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-// Using prepend to have text at the top
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//
+// All the Resume Sections
+//
 
-// var skills = ["HTML", "CSS", "JavaScript", "Python", "Django"];
-// $("#main").append(skills);
-// $("#main").append(skills[3]);
-// $("#main").append(skills.length);
+// Work in JSON format
+var work = {
+    "jobs" : [
+        {
+            "employer" : "Guidewire",
+            "title" : "Sr. Systems/Software Engineer",
+            "location" : "Foster City, CA",
+            "dates" : "2015 - 2017",
+            "description" : "Developed App from scratch for automatic VMs creation on demand, managing, and deleting after use to save resources. Worked on Self-Service Web App that helps users to Create/Deploy VMs on their own."
+        },
+        {
+            "employer" : "Pro Unlimited",
+            "title" : "Sr. Systems/Software Engineer",
+            "location" : "Burlingame, CA",
+            "dates" : "2012 - 2015",
+            "description" : "Worked on resolving Data Center challenges. Researched and presented to management the solutions by Virtualization and Cloud. The solutions reduced cost, saved time in deploying, and easy management."
+        },
+        {
+            "employer" : "Riverbed",
+            "title" : "Sr. Systems/Software Engineer",
+            "location" : "San Francisco, CA",
+            "dates" : "2010 - 2012",
+            "description" : "Architected a new design and development that was much easier to interface using Web Application and was understood by all teams. Lead to 30% reduction in time and higher quality of systems."
+        },
+        {
+            "employer" : "GridNet",
+            "title" : "Sr. Software Engineer",
+            "location" : "San Francisco, CA",
+            "dates" : "2010",
+            "description" : "Worked on Smart Meters Systems and Software to improve quality & performance. This led to greater throughput (up 35%) and fewer bug escapes (down 58%), with much higher customer satisfaction (up 38%)."
+        },
+        {
+            "employer" : "Sun Microsystems",
+            "title" : "Member of Technical Staff",
+            "location" : "Menlo Park/Newark/Santa Clara, CA",
+            "dates" : "2001 - 2010",
+            "description" : "Worked on Sun’s Enterprise Server and Software (ESS) which was a critical component of a new offering. Released with high quality which led to became company’s top revenue-generating product."
+        },
+    ]
+};
+
+// Project in JSON format
+var projects = {
+    "projects" : [
+        {
+            "title" : "Neighborhood Map",
+            "dates" : "2017",
+            "description" : "Goal of the project is to develop a Single-page, Responsive Web Application that loads highlighted locations in a neighborhood, so Users can know about them.",
+            "images" : ["https://github.com/panilkumar16/neighborhood-map"]
+        },
+        {
+            "title" : "Item Catalog App",
+            "dates" : "2017",
+            "description" : "Purpose of the Web Application is Users around the World can securely login to contribute to an organized list of Categories and its Items.",
+            "images" : ["https://github.com/panilkumar16/item-catalog"]
+        }
+    ]
+};
+
+// Biography in object
+var bio = {
+    "name" : "Anilkumar P",
+    "role" : "Full Stack Software Engineer & Lead",
+    "welcomeMessage" : "Welcome to Anilkumar Biography",
+    "bioPic" : "images/pic.jpg",
+    "contacts" : {
+        "mobile" : "510-640-6156",
+        "email" : "panilkumar16@gmail.com",
+        "github" : "panilkumar16",
+        "location" : "San Francisco Bay Area"
+    },
+    "skills" : [
+        "HTML", "CSS", "JavaScript", "Python", "Django"
+    ]
+};
+
+// Education in JSON format
+var education = {
+    "schools" : [
+        {
+            "name" : "Udacity",
+            "location" : "Palo Alto, CA",
+            "degree_dates" : 2017,
+            "url" : "https://www.udacity.com/",
+            "majors" : ["Full Stack Web Developer"]
+        },
+        {
+            "name" : "Cal State East Bay",
+            "location" : "Hayward, CA",
+            "degree_dates" : 2007,
+            "majors" : ["Hi Performance Management"],
+            "url" : "http://www.csueastbay.edu/"
+        },
+        {
+            "name" : "San Jose State University",
+            "location" : "San Jose, CA",
+            "degree_dates" : 2005,
+            "majors" : ["Software Engineering"],
+            "url" : "http://www.sjsu.edu/"
+        },
+        {
+            "name" : "Dayananda Sagar College of Engineering",
+            "location" : "Bangalore, KA",
+            "degree_dates" : 1997,
+            "majors" : ["Computer Science and Engineering"],
+            "url" : "http://dayanandasagar.edu/"
+        }
+    ],
+    "onlineCourses" : [
+        {
+            "title" : "JavaScript Syntax",
+            "school" : "Udacity",
+            "dates" : 2017,
+            "url" : "http://www.udacity.com/course/ud804"
+        }
+    ]
+};
--- js/test.js
+++ js/test.js
@@ -1,53 +1,161 @@
-// $("#main").append("Anilkumar P");
+$("#main").append("Anilkumar P");
 
-// var firstName = "Anilkumar";
-// var age = 32;
-// console.log(firstName);
+var firstName = "Anilkumar";
+var age = 32;
+console.log(firstName);
 
-// var awesomeThoughts = "I am Anilkumar and I am AWESOME!";
-// console.log(awesomeThoughts);
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-// console.log(funThoughts);
-// $("#main").append(funThoughts);
+var awesomeThoughts = "I am Anilkumar and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
+$("#main").append(funThoughts);
 
-// var email = "panilkumar16@hotmail.com";
-// var newEmail = email.replace("hotmail", "gmail");
-// console.log(email);
-// console.log(newEmail);
+var email = "panilkumar16@hotmail.com";
+var newEmail = email.replace("hotmail", "gmail");
+console.log(email);
+console.log(newEmail);
 
-// var s = "audacity";
+var s = "audacity";
 
-// var udacityizer = function(s) {  
-//     // Right now, the variable s === "audacity"
-//     // Manipulate s to make it equal to "Udacity"
-//     // Your code goes here!
-//     s = 'U' + s.slice(2);
-//     return s;
-// };
+var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    s = 'U' + s.slice(2);
+    return s;
+};
 
-// // Did your code work? The line below will tell you!
-// console.log(udacityizer(s));
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
 
-// var sampleArray = [0,0,7];
+var sampleArray = [0,0,7];
 
-// var incrementLastArrayElement = function(_array) {
-//     var newArray = [];
-//     // Your code should make newArray equal to an array that has the same
-//     // values as _array, but the last number has increased by one.
+var incrementLastArrayElement = function(_array) {
+    var newArray = [];
+    // Your code should make newArray equal to an array that has the same
+    // values as _array, but the last number has increased by one.
     
-//     // For example:
-//     // _array = [1, 2, 3];
-//     // turns into:
-//     // newArray = [1, 2, 4];
+    // For example:
+    // _array = [1, 2, 3];
+    // turns into:
+    // newArray = [1, 2, 4];
     
-//     // Your code goes in here!
-//     last_index = _array.length - 1
-//     _array[last_index] += 1
-//     newArray = _array
+    // Your code goes in here!
+    last_index = _array.length - 1
+    _array[last_index] += 1
+    newArray = _array
     
-//     // Don't delete this line!
-//     return newArray;
-// };
+    // Don't delete this line!
+    return newArray;
+};
 
-// // Did your code work? The line below will tell you!
-// console.log(incrementLastArrayElement(sampleArray));
+// Did your code work? The line below will tell you!
+console.log(incrementLastArrayElement(sampleArray));
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    var name = oldName.split(" ");
+    name[0] = name[0].charAt(0).toUpperCase() + name[0].slice(1).toLowerCase();
+    name[1] = name[1].toUpperCase();
+    finalName = name.join(" ");
+    
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
+
+var name = "Anilkumar P";
+var role = "Full Stack Software Engineer & Lead";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+// Using prepend to have text at the top
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["HTML", "CSS", "JavaScript", "Python", "Django"];
+$("#main").append(skills);
+$("#main").append(skills[3]);
+$("#main").append(skills.length);
+
+// Declare and initialize object
+var bio = {
+    "name" : "Anilkumar P",
+    "role" : "Full Stack Software Engineer & Lead",
+    "contacts" : {
+        "mobile" : "510-640-6156",
+        "email" : "panilkumar16@gmail.com",
+        "location" : "San Francisco Bay Area"
+    }, 
+    "welcomeMessage" : "Welcome",
+    "skills" : [
+        "HTML", "CSS", "JavaScript", "Python", "Django"
+    ],
+    "bioPic" : "images/pic.jpg"
+};
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contacts.email);
+$("#main").append(bio.contacts.mobile);
+$("#main").append(bio.welcomeMessage);
+$("#main").append(bio.skills);
+
+
+// Dot notation
+bio.city = "San Mateo"
+// Bracket notation
+bio["state"] = "California"
+
+$("#main").append(bio.city);
+$("#main").append(bio.state);
+$("#main").append(bio["state"]);
+
+// Work detials in dot notation
+var work = {};
+work.current_job_position = "Sr. Systems/Software Engineer",
+work.employer = "Guidewire",
+work.years_worked = 2,
+work.city = "Foster City"
+$("#main").append(work.current_job_position);
+
+// Education detials in bracket notation
+var education = {};
+education["last_school_name"] = "Udacity";
+education["years_attended"] = 1;
+education["city"] = "Palo Alto";
+$("#main").append(education["last_school_name"]);
+
+// Education in object notation JSON
+var edu = {
+    "schools" : [
+        {
+            "name" : "Udacity",
+            "location" : "Palo Alto, CA",
+            "degree" : "Nanodegree",
+            "majors" : ["Full Stack Web Developer"],
+            "dates" : 2017,
+            "url" : "https://www.udacity.com/"
+        },
+        {
+            "name" : "Cal State East Bay",
+            "location" : "Hayward, CA",
+            "degree" : "Hi Performance Management Course",
+            "majors" : ["Management"],
+            "dates" : 2017,
+            "url" : "http://www.csueastbay.edu/"
+        }
+    ],
+    "onlineCourses" : [
+        {
+            "title" : "JavaScript Syntax",
+            "school" : "Udacity",
+            "dates" : 2017,
+            "url" : "http://www.udacity.com/course/ud804"
+        }
+    ]
+};
